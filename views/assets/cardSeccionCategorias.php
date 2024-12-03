49e37868ed5fc48a2a553c4fd65d1196745687a3
Marc Santiago <marcsantiagofp@ibf.cat>
2024-12-03 20:26:46 +0100

    modified:   views/assets/cardPostres.php 	modified:   views/assets/cardRecomendado.php 	modified:   views/assets/cardSeccionCategorias.php

--- views/assets/cardSeccionCategorias.php
+++ views/assets/cardSeccionCategorias.php
@@ -1,8 +1,10 @@
-<?php foreach ($categorias as $item): ?>
-    <div class="col-6 col-md-3 col-lg">
-        <a href="?controller=producto&action=mostrarProductos&id=<?= $item->getId(); ?>" class="nombre-categorias text-decoration-none">
-            <img src="<?= $item->getUrl_imagen(); ?>" class="img-fluid">
-            <p><?= $item->getNombre(); ?></p>
-        </a>
-    </div>
-<?php endforeach; ?>
+<div class="row gx-3 gy-4">
+    <?php foreach ($categorias as $item): ?>
+        <div class="col-6 col-md-3 col-lg">
+            <a href="?controller=producto&action=mostrarProductos&id=<?= $item->getId(); ?>" class="nombre-categorias text-decoration-none">
+                <img src="<?= $item->getUrl_imagen(); ?>" class="img-fluid">
+                <p><?= $item->getNombre(); ?></p>
+            </a>
+        </div>
+    <?php endforeach; ?>
+</div>
