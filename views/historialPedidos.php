79c1010090cc2da0cc7411baac3d8d5d6ae1e501
Marc Santiago <marcsantiagofp@ibf.cat>
2024-12-19 18:59:08 +0100

    modified:   views/historialPedidos.php

--- views/historialPedidos.php
+++ views/historialPedidos.php
@@ -4,8 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mis Compras</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="styles.css"> <!-- Enlace al archivo CSS externo -->
 </head>
 <body class="bg-light">
     <div class="container">
